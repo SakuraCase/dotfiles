28b7129a7bc62e619147dff943657b5d3e9fb203
SakuraCase <sakurada6617@gmail.com>
2016-12-18 23:04:16 +0900

    update

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-path=`pwd`
+path=$(cd $(dirname $0) && pwd)
 
 ln -sf $path/.vimrc ~/.vimrc
-ln -sf $path/.vim/colors ~/.vim
-ln -sf $path/.vim/rc ~/.vim
+ln -sf $path/.vim/colors ~/.vim/
+ln -sf $path/.vim/rc ~/.vim/
+ln -sf $path/.vim/syntax ~/.vim/
+ln -sf $path/.config/flake8 ~/.config/
