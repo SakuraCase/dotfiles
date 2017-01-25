b4ba203f0c37cbe199ef076f4cfa5f3c55a32fbc
SakuraCase <sakurada6617@gmail.com>
2017-01-25 21:44:53 +0900

    update

--- .vim/vimrc/basic.vim
+++ .vim/vimrc/basic.vim
@@ -12,7 +12,6 @@ set noexpandtab
 set tabstop=4
 set shiftwidth=4
 set autoindent
-set smartindent
 
 " 行数表示
 set number
--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -4,5 +4,6 @@ files=(.vimrc .vim/colors .vim/rc .vim/syntax .vim/vimrc .config/flake8)
 path=$(cd $(dirname $0) && pwd)
 
 for file in ${files[@]}; do
-	ln -sf $path/$file ~/$file
+	unlink ~/$file
+	ln -s $path/$file ~/$file
 done
